8b525ced355aba28d37190fe6bbcddc597a6d64c
Ginok1 <lennart.koenig@neptun.uni-freiburg.de>
2025-02-26 12:36:33 +0100

    fix imports

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='tartanair_tools',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[],  # Add dependencies if needed
-)
